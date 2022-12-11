23c0a4d65b80ae8885d8ab0128f1733a822eb3f9
HaXr <haxr@outlook.dk>
2022-12-11 05:00:52 +0100

    Start script now copies the server.properties file from a folder instead of downloading it

--- Server Pack/start.bat
+++ Server Pack/start.bat
@@ -2,6 +2,6 @@ set MAX_RAM=5G
 IF EXIST "server.properties" (
 	java -Xmx%MAX_RAM% -jar fabric-server-mc.1.18.2-loader.0.14.11-launcher.0.11.1.jar nogui
 	) ELSE (
-	curl -L -o server.properties https://www.dropbox.com/s/s8y55y1lzyegrzd/server.properties?dl=1
+	copy default\server.properties
 	java -Xmx%MAX_RAM% -jar fabric-server-mc.1.18.2-loader.0.14.11-launcher.0.11.1.jar nogui
 	)
