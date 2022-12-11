6f3424453e82c983f19bfae603aae11f649c1b5c
HaXr <haxr@outlook.dk>
2022-12-11 04:58:33 +0100

    Update start.bat

--- Server Pack/start.bat
+++ Server Pack/start.bat
@@ -1,7 +1,7 @@
 set MAX_RAM=5G
 IF EXIST "server.properties" (
 	java -Xmx%MAX_RAM% -jar fabric-server-mc.1.18.2-loader.0.14.11-launcher.0.11.1.jar nogui
-	)
-else
+	) ELSE (
 	curl -L -o server.properties https://www.dropbox.com/s/s8y55y1lzyegrzd/server.properties?dl=1
 	java -Xmx%MAX_RAM% -jar fabric-server-mc.1.18.2-loader.0.14.11-launcher.0.11.1.jar nogui
+	)
