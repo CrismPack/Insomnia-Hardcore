c24b8c62848815ff7cb4019eac01b03b4107f961
HaXr <haxr@outlook.dk>
2023-04-25 02:02:05 +0200

    Updated serverpack to use latest fabric

--- Server Pack/start.bat
+++ Server Pack/start.bat
@@ -1,7 +1,7 @@
 set MAX_RAM=5G
 IF EXIST "server.properties" (
-	java -Xmx%MAX_RAM% -jar fabric-server-mc.1.18.2-loader.0.14.18-launcher.0.11.2.jar nogui
+	java -Xmx%MAX_RAM% -jar fabric-server-mc.1.18.2-loader.0.14.19-launcher.0.11.2.jar nogui
 	) ELSE (
 	copy default\server.properties
-	java -Xmx%MAX_RAM% -jar fabric-server-mc.1.18.2-loader.0.14.18-launcher.0.11.2.jar nogui
+	java -Xmx%MAX_RAM% -jar fabric-server-mc.1.18.2-loader.0.14.19-launcher.0.11.2.jar nogui
 	)
