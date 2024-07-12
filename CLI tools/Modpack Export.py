f6e692b30663553c7c88f9d4dfcb5a2e26d6b6bf
HaXr <haxr@outlook.dk>
2024-07-13 00:21:04 +0200

    Implemented automated(ish) server export & bcc updating for the server
    Also did some cleanup of the code

--- CLI tools/Modpack Export.py
+++ CLI tools/Modpack Export.py
@@ -1,7 +1,8 @@
 import os
+import os.path
 import json
 import subprocess
-from shutil import rmtree, make_archive, move, copyfile
+from shutil import rmtree, make_archive, move, copytree
 from pathlib import Path
 
 import toml  # pip install toml
@@ -10,93 +11,46 @@ from mdutils.mdutils import MdUtils
 from mdutils import Html
 
 
+##########################################################
+# Configuration
+
+export_client = True
+export_server = True
+
+refresh_only = False
+update_bcc_version = True
+cleanup_temp = True
+create_release_notes = True
+server_mods_remove_list = ["From-The-Fog-1.20-v1.9.2-Forge-Fabric.jar"]
+print_path_debug = True
+
+##########################################################
+# Variables
 user_path = os.path.expanduser("~")
 
 modpack_name = "InsomniaHardcore"
 minecraft_version = "1.20.1"
-packwiz_side = "server"
 
 # Get path of project dynamically.
 script_path = __file__
 git_path = str(os.path.dirname(os.path.dirname(script_path))).replace("/","\\") # .replace("/","\\") is to ensure that the path will be in the Windows format.
 
-print("[DEBUG] " + git_path)
-
 packwiz_path = git_path + "\\Packwiz\\"
 serverpack_path = git_path + "\\Server Pack\\"
 packwiz_exe_path = os.path.expanduser("~") + "\\go\\bin\\packwiz.exe"
 packwiz_manifest = "pack.toml"
-packwiz_installer_path = git_path + "\\CLI tools\\packwiz-installer-bootstrap.jar"
-bcc_config_path = packwiz_path + "config\\bcc.json"
-
-mmc_config = packwiz_path + "mmc-export.toml"
+bcc_client_config_path = packwiz_path + "config\\bcc.json"
+bcc_server_config_path = serverpack_path + "config\\bcc.json"
 export_path = git_path + "\\Export\\"
-
-mmc_cache_path = packwiz_path + "mmc-cache\\"
-mmc_dotminecraft_path = mmc_cache_path + ".minecraft\\"
-mmc_input_path = packwiz_path + "mcc-cache.zip"
-
 tempfolder_path = export_path + "temp\\"
+temp_mods_path = tempfolder_path + "mods\\"
 
-print("[DEBUG] " + git_path)
-print("[DEBUG] " + packwiz_path)
-print("[DEBUG] " + packwiz_exe_path)
-print("[DEBUG] " + packwiz_installer_path)
-print("[DEBUG] " + bcc_config_path)
-print("[DEBUG] " + mmc_config)
-print("[DEBUG] " + mmc_cache_path)
-print("[DEBUG] " + mmc_dotminecraft_path)
-print("[DEBUG] " + mmc_input_path)
-
-
-
-refresh_only = False
-gh_login = False
-export_mmc_modrinth = False
-export_mmc_curseforge = False
-update_bcc_version = True
-cleanup_cache = False
-move_disabled_mods = True
-test_linux_mappings = False
-create_release_notes = True
-
-
-# NO_GUI_BOOTSTRAP = False
-# USE_BOOTSTRAP = False
-
-export_client = True
-export_server = True
-
-
-# def clear_mmc_cache(path):
-#     os.chdir(path)
-#     retain = ["packwiz-installer.jar"] # Files that shouldn't be deleted
-    
-#     # Loop through everything in folder in current working directory
-#     for item in os.listdir(os.getcwd()):
-#         if item not in retain:  # If it isn't in the list for retaining
-#             try:
-#                 os.remove(item)  # Remove the item
-#             except:
-#                 pass
-#             try:
-#                 rmtree(item)
-#             except:
-#                 pass
-
-def clear_temp(path):
-    os.chdir(path)
-    
-    # Loop through everything in folder in current working directory
-    for item in os.listdir(os.getcwd()):
-        try:
-            os.remove(item)  # Remove the item
-        except:
-            pass
-        try:
-            rmtree(item)
-        except:
-            pass
+if print_path_debug:
+    print("[DEBUG] " + git_path)
+    print("[DEBUG] " + packwiz_path)
+    print("[DEBUG] " + packwiz_exe_path)
+    print("[DEBUG] " + bcc_client_config_path)
+    print("[DEBUG] " + bcc_server_config_path)
 
 
 def main():
@@ -107,10 +61,6 @@ def main():
         pack_toml = toml.load(f)
     pack_version = pack_toml["version"]
     
-
-    # Used for authenticating with GitHub for faster API responses.
-    if gh_login:
-        subprocess.call("mmc-export gh-login")
     
     # Parse the related changelog file for overview details and create release markdown files for CF and MR.
     if create_release_notes:
@@ -146,51 +96,45 @@ def main():
         mdFile_MR.new_paragraph(md_element_crism_spacer)
         mdFile_MR.create_md_file()
         
-        os.chdir(packwiz_path)
-        
 
     if not refresh_only:
         
+        os.chdir(packwiz_path)
+
         # Update version number in BCC
         if update_bcc_version:
-            with open(bcc_config_path, "r") as f:
+            # Client
+            with open(bcc_client_config_path, "r") as f:
                 bcc_json = json.load(f)
             bcc_json["modpackVersion"] = pack_version
-            with open(bcc_config_path, "w") as f:
+            with open(bcc_client_config_path, "w") as f:
+                json.dump(bcc_json, f)
+            # Server
+            with open(bcc_server_config_path, "r") as f:
+                bcc_json = json.load(f)
+            bcc_json["modpackVersion"] = pack_version
+            with open(bcc_server_config_path, "w") as f:
                 json.dump(bcc_json, f)
 
-        # if move_disabled_mods:
-        #     mods_path = packwiz_path + "mods\\"
-        #     disabled_mods_path = mods_path + "disabled\\"
-        #     if os.name == "posix":
-        #         mods_path = mods_path.replace("\\","/")
-        #         disabled_mods_path = disabled_mods_path.replace("\\","/")
-        #     os.chdir(mods_path)
-            
-        #     # Parse mod toml files for (disabled) marker.
-        #     for item in os.listdir():
-        #         try:
-        #             with open(item, "r") as f:
-        #                 mod_toml = toml.load(f)
-        #                 if "disabled" in mod_toml["side"]:
-        #                     f.close()
-        #                     move(item, disabled_mods_path)
 
-        #         except:
-        #             print("[DEBUG] Error occured in disabled parser")
-
-
-
+        ##########################################################
+        # Export client pack
         os.chdir(packwiz_path)
+
         # Refresh the packwiz index
         subprocess.call(f"{packwiz_exe_path} refresh", shell=True)
 
+        # Packwiz exporting
         file = f'InsomniaHardcore-{minecraft_version}-{pack_version}.zip'
         if export_client:
             # Export CF modpack using Packwiz.
             subprocess.call(f"{packwiz_exe_path} cf export", shell=True)
             move(file, f"{export_path}{file}")
             print("[PackWiz] Client exported.")
+
+
+        ##########################################################
+        # Export server pack
 
         if export_server:
             # Export CF modpack using Packwiz.
@@ -199,91 +143,30 @@ def main():
             move(file, f"{export_path}{file_server_name}")
             print("[PackWiz] Server exported.")
 
+            os.chdir(git_path)
+            # Deletes the temp folder if it already exists.
+            if os.path.isdir(tempfolder_path):
+                rmtree(tempfolder_path)
 
-        os.chdir(serverpack_path)
+            copytree("Server Pack", tempfolder_path) # Copies contents of "Server Pack" folder into the temp folder.
 
-        # Moves override folders into temp folder folder
-        clear_temp(tempfolder_path)
-        move_exclude_list = [""]
-        for item in os.listdir(os.getcwd()):
-            if item not in move_exclude_list:
-                copyfile(item, tempfolder_path)
+            # Console input.
+            server_mods_path = input(f'Create a new modpack instance in the CurseForge launcher using the {modpack_name}-ServerPack-{minecraft_version}-{pack_version}.zip file. Then drag the mods folder from that instance into the terminal (No spaces allowed for the source directory): ')
+            
+            copytree(server_mods_path, temp_mods_path, dirs_exist_ok=True)
+            
+            # Removes specified files from mods folder
+            os.chdir(temp_mods_path)
+            for file in os.listdir():
+                if file in server_mods_remove_list:
+                    os.remove(file)
 
-        #os.chdir(export_path)
-        #make_archive(f"{modpack_name}-ServerPack-{minecraft_version}-{pack_version}", 'zip', export_path) # Creates mcc-cache.zip file based on mmc-cache folder.
+            os.chdir(export_path)
+            make_archive(f"{modpack_name}-ServerPack-{minecraft_version}-{pack_version}", 'zip', tempfolder_path)
 
-        # # Creates mmc-cache folder if it doesn't already exist and ensure that it is empty.
-        # try:
-        #     os.mkdir(mmc_cache_path)
-        # except:
-        #     pass
-        # clear_mmc_cache(mmc_cache_path)
-
-
-        # file = Path(mmc_cache_path + "packwiz-installer.jar")
-        
-        # if NO_GUI_BOOTSTRAP and USE_BOOTSTRAP:
-        #     if file.is_file():
-        #         # Export Packwiz modpack to MMC cache folder and zip it.
-        #         subprocess.call(f"java -jar \"{packwiz_installer_path}\" -s {packwiz_side} \"{packwiz_path + packwiz_manifest}\" -g --bootstrap-no-update", shell=True)
-        #     else:
-        #         # Export Packwiz modpack to MMC cache folder and zip it.
-        #         subprocess.call(f"java -jar \"{packwiz_installer_path}\" -s {packwiz_side} \"{packwiz_path + packwiz_manifest}\" -g", shell=True)
-        # elif not NO_GUI_BOOTSTRAP and USE_BOOTSTRAP:
-        #     if file.is_file():
-        #         # Export Packwiz modpack to MMC cache folder and zip it.
-        #         subprocess.call(f"java -jar \"{packwiz_installer_path}\" -s {packwiz_side} \"{packwiz_path + packwiz_manifest}\" --bootstrap-no-update", shell=True)
-        #     else:
-        #         # Export Packwiz modpack to MMC cache folder and zip it.
-        #         subprocess.call(f"java -jar \"{packwiz_installer_path}\" -s {packwiz_side} \"{packwiz_path + packwiz_manifest}\"", shell=True)
-
-        # # Creates mmc\.minecraft folder if it doesn't already exist.
-        # try:
-        #     os.mkdir(mmc_dotminecraft_path)
-        # except:
-        #     pass
-        
-
-        # # Moves override folders into .minecraft folder
-        # move_list = ["shaderpacks", "resourcepacks", "mods", "config"]
-        # for item in os.listdir(os.getcwd()):
-        #     if item in move_list:
-        #         move(item, mmc_dotminecraft_path)
-
-        
-        # if move_disabled_mods:
-        #     os.chdir(disabled_mods_path)
-        #     retain = [".gitkeep"] # Files that shouldn't be deleted
-        #     try:
-        #         # Moves disabled mods back.
-        #         for item in os.listdir():
-        #             if item not in retain:
-        #                 move(item, mods_path)
-        #     except OSError as e:
-        #         print(e)
-        #     os.chdir(packwiz_path)
-        
-        
-        # make_archive("mcc-cache", 'zip', mmc_cache_path) # Creates mcc-cache.zip file based on mmc-cache folder.
-
-        # # Export CurseForge modpack using MMC method.
-        # if export_mmc_curseforge:
-        #     print("[MMC] Exporting CurseForge...")
-        #     args = (
-        #         "mmc-export",
-        #         "--input", mmc_input_path,
-        #         "--format", "CurseForge",
-        #         "-o", cf_export_path,
-        #         "-c", mmc_config,
-        #         "-v", pack_version,
-        #         "--scheme", modpack_name + "-" + minecraft_version + "-{version}",
-        #     ); subprocess.call(args, shell=True)
-        #     print("[MMC] CurseForge exported.")
-        
-        # if cleanup_cache:
-        #     os.remove("mcc-cache.zip")
-        #     clear_mmc_cache(mmc_cache_path)
-        #     print("Cache cleanup finished.")
+        if cleanup_temp and os.path.isdir(tempfolder_path):
+            rmtree(tempfolder_path)
+            print("Temp folder cleanup finished.")
         
         os.chdir(packwiz_path)
         subprocess.call(f"{packwiz_exe_path} refresh", shell=True)
